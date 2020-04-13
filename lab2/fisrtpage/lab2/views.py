e05875687798a9d3dd9ba8fcd69b82fd799e973f
Ivan Kovalev <admin@MacBook-Pro-Ivan.local>
2020-04-13 12:53:56 +0300

    sync

--- lab2/fisrtpage/lab2/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
-from django import template
-def home(request):
-    return render(request, 'templates/static_handler.html')
-# Create your views here.
