f9e0ea2c2922e27d59ccae50c0b62e62e7d7fd13
Ivan Kovalev <shoker444@gmail.com>
2020-06-05 14:32:16 +0300

    Added pract 3

---
+++ pract3/pract3_2.java
@@ -0,0 +1,11 @@
+public class pract3_2{
+	public static void main(String[] args){
+	System.out.println(findZip("all zip files are zipped"));
+	}
+	public static int findZip(String fz) {
+		if (fz.lastIndexOf("zip") != fz.indexOf("zip")&& fz.indexOf("zip") != -1 ) {
+			return fz.lastIndexOf("zip");
+		}
+		return -1;
+	}
+}
---
+++ pract3/pract3_4.java
@@ -0,0 +1,18 @@
+public class pract3_4{
+	public static void main(String[] args){
+	System.out.println(flipEndChars("Cat, dog, and mouse."));
+	}
+public static String flipEndChars(String s) {
+		if(s.length()<2){
+			return "Incompatible.";
+		}
+		if(s.charAt(0)==s.charAt(s.length()-1)){
+			return "Two's a pair.";
+		}
+		char[] chars = s.toCharArray();
+		char a=chars[0];
+		chars[0]=chars[s.length()-1];
+		chars[s.length()-1]=a;
+		return String.valueOf(chars);
+	}
+}
